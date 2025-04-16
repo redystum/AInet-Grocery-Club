1ec791ce17b651836e75d11df0ac6672a030910a
Pedro Silva <2230997@my.ipleiria.pt>
2025-04-16 18:39:20 +0100

    Criei o modelo Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,10 +1,17 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Models\Product; // Certifique-se de ter o modelo Product
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        // Busca todos os produtos do banco de dados
+        $products = Product::all();
+
+        // Retorna a view com os produtos
+        return view('pages.products', compact('products'));
+    }
 }
