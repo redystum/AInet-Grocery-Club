95d8d283e28494f22deef78d73072cc835529746
Daniel Nemchynov <2230943@my.ipleiria.pt>
2025-04-29 16:01:19 +0100

    A opção "products" da navbar ja esta a funcionar!

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -17,8 +17,10 @@
                        class="{{ Route::currentRouteName() === 'home' ? 'text-indigo-600 dark:text-indigo-400 border-b-2 border-indigo-600 dark:border-indigo-400' : 'text-gray-700 dark:text-neutral-300 hover:text-indigo-600 dark:hover:text-indigo-400' }} px-3 py-2 text-sm font-medium transition duration-300">
                         Home
                     </a>
-                    <a href="#"
-                       class="text-gray-700 dark:text-neutral-300 hover:text-indigo-600 dark:hover:text-indigo-400 px-3 py-2 text-sm font-medium transition duration-300">Products</a>
+                    <a href="{{ route('products.index') }}"
+                       class="{{ Route::currentRouteName() === 'products.index' ? 'text-indigo-600 dark:text-indigo-400 border-b-2 border-indigo-600 dark:border-indigo-400' : 'text-gray-700 dark:text-neutral-300 hover:text-indigo-600 dark:hover:text-indigo-400' }} px-3 py-2 text-sm font-medium transition duration-300">
+                        Products
+                    </a>
                     <a href="#"
                        class="text-gray-700 dark:text-neutral-300 hover:text-indigo-600 dark:hover:text-indigo-400 px-3 py-2 text-sm font-medium transition duration-300">Contacts</a>
                     <a href="#"
