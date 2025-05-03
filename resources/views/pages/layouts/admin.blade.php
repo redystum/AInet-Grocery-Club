67acf0fa93e8564f5993b4ddfa407ee37082b24b
redystum <rffarffarffa@gmail.com>
2025-05-03 17:17:30 +0100

    admin navbar

--- resources/views/pages/layouts/admin.blade.php
+++ resources/views/pages/layouts/admin.blade.php
@@ -16,14 +16,15 @@
 
 <body class="bg-white dark:bg-neutral-900 text-gray-900 dark:text-neutral-100">
 
-{{--<x-adm-navbar/>--}}
+<x-admin.navbar />
 
-<main>
+<main class="lg:ml-64 pt-16 lg:pt-0 transition-all duration-300" id="mainContent">
     @yield('content')
 </main>
 
 {{--    <x-footer/>--}}
 
+@vite(['resources/js/adminNavBar.js'])
 </body>
 
 </html>
