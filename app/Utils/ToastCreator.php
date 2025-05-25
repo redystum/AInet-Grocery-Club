81fc815b325a07aeffbd7383c53e5e1efe94392f
redystum <rffarffarffa@gmail.com>
2025-05-25 16:40:54 +0100

    refactor: update toast methods to have a default time parameter

--- app/Utils/ToastCreator.php
+++ app/Utils/ToastCreator.php
@@ -9,16 +9,16 @@ class ToastCreator
      *
      * @param string $message
      * @param string $title
-     * @param int|null $time
+     * @param int $time
      * @return void
      */
-    public static function success(string $message, string $title = 'Success', ?int $time = null): void
+    public static function success(string $message, string $title = 'Success', int $time = 5000): void
     {
         session()->flash('toast', [
             'title' => $title,
             'message' => $message,
             'type' => 'success',
-            'time' => $time ?? 5000,
+            'time' => $time,
         ]);
     }
 
@@ -27,16 +27,16 @@ class ToastCreator
      *
      * @param string $message
      * @param string $title
-     * @param int|null $time
+     * @param int $time
      * @return void
      */
-    public static function error(string $message, string $title = 'Error', ?int $time = null): void
+    public static function error(string $message, string $title = 'Error', int $time = 5000): void
     {
         session()->flash('toast', [
             'title' => $title,
             'message' => $message,
             'type' => 'error',
-            'time' => $time ?? 5000,
+            'time' => $time,
         ]);
     }
 
@@ -45,16 +45,16 @@ class ToastCreator
      *
      * @param string $message
      * @param string $title
-     * @param int|null $time
+     * @param int $time
      * @return void
      */
-    public static function warn(string $message, string $title = 'Warning', ?int $time = null): void
+    public static function warn(string $message, string $title = 'Warning', int $time = 5000): void
     {
         session()->flash('toast', [
             'title' => $title,
             'message' => $message,
             'type' => 'warning',
-            'time' => $time ?? 5000,
+            'time' => $time,
         ]);
     }
 
@@ -63,16 +63,16 @@ class ToastCreator
      *
      * @param string $message
      * @param string $title
-     * @param int|null $time
+     * @param int $time
      * @return void
      */
-    public static function info(string $message, string $title = 'Info', ?int $time = null): void
+    public static function info(string $message, string $title = 'Info', int $time = 5000): void
     {
         session()->flash('toast', [
             'title' => $title,
             'message' => $message,
             'type' => 'info',
-            'time' => $time ?? 5000,
+            'time' => $time,
         ]);
     }
 }
