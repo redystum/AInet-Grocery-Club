52ba29f2b6c7e33fe260b89ba96013a931e360d6
redystum <rffarffarffa@gmail.com>
2025-06-14 21:33:49 +0100

    remove cart link from navbar for improved navigation

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -87,11 +87,6 @@
                                     <i class="fas fa-dashboard mr-3 text-indigo-500 dark:text-indigo-400 w-4"></i>
                                     Dashboard
                                 </a>
-                                <a href="#"
-                                   class="flex items-center px-4 py-2 text-sm text-gray-700 dark:text-neutral-300 hover:bg-gray-100 dark:hover:bg-neutral-700">
-                                    <i class="fas fa-shopping-cart mr-3 text-indigo-500 dark:text-indigo-400 w-4"></i>
-                                    Cart
-                                </a>
                                 <a href="{{ route("logout") }}"
                                    class="flex items-center rounded-b-xl px-4 py-2 text-sm text-gray-700 dark:text-neutral-300 hover:bg-gray-100 dark:hover:bg-neutral-700">
                                     <i class="fas fa-sign-out-alt mr-3 text-indigo-500 dark:text-indigo-400 w-4"></i>
@@ -146,8 +141,6 @@
             <a href="#"
                class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 dark:text-neutral-300 hover:text-indigo-600 dark:hover:text-indigo-400 hover:bg-gray-100 dark:hover:bg-neutral-800">Products</a>
             <a href="#"
-               class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 dark:text-neutral-300 hover:text-indigo-600 dark:hover:text-indigo-400 hover:bg-gray-100 dark:hover:bg-neutral-800">Contacts</a>
-            <a href="#"
                class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 dark:text-neutral-300 hover:text-indigo-600 dark:hover:text-indigo-400 hover:bg-gray-100 dark:hover:bg-neutral-800">About
                 us</a>
         </div>
@@ -189,11 +182,6 @@
                             Management
                         </a>
                     @endif
-                    <a href="#"
-                       class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 dark:text-neutral-300 hover:text-indigo-600 dark:hover:text-indigo-400 hover:bg-gray-100 dark:hover:bg-neutral-800">
-                        <i class="fas fa-shopping-cart mr-3 text-indigo-500 dark:text-indigo-400"></i>
-                        Cart
-                    </a>
                     <a href="{{ route('dashboard') }}"
                        class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 dark:text-neutral-300 hover:text-indigo-600 dark:hover:text-indigo-400 hover:bg-gray-100 dark:hover:bg-neutral-800">
                         <i class="fas fa-dashboard mr-3 text-indigo-500 dark:text-indigo-400"></i>
